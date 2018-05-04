35569c42cc239ad802771a1459f739e12ad2ffc9
Aleksander <adutkowski@sztajfa.cc>
2018-05-04 19:32:29 +0200

    Add config for public firebase test server

---
+++ src/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDtiJdRXpJZKBzq2Fp-be5q64sGw_DZwDc",
+    authDomain: "ng-fire-models.firebaseapp.com",
+    databaseURL: "https://ng-fire-models.firebaseio.com",
+    projectId: "ng-fire-models",
+    storageBucket: "ng-fire-models.appspot.com",
+    messagingSenderId: "811900461469"
+  }
+};
